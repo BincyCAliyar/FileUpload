b9e6187892c9394bf8fc3e5c65ea16b4d9692127
BincyCAliyar <bincycaliyar1993@gmail.com>
2021-07-28 09:57:16 +0530

    Initial Commit

---
+++ File Upload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  File Upload
+//
+//  Created by Sijo Thadathil on 28/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
